7cef061c58f60d292a03ad38abe327049fba29d2
d.alexanov <d.alexanov@gmail.com>
2016-05-23 14:44:25 +0300

    Grid layout manipulations demo

---
+++ demo/grid-layout-manipulations/grid-layout-manipulations.js
@@ -0,0 +1,74 @@
+angular.module('application', ['ui.scroll', 'ui.scroll.jqlite', 'ui.scroll.grid'])
+	.controller('gridController', [
+		'$scope', '$log', '$timeout', function ($scope, console, $timeout) {
+			var datasource = {};
+
+			datasource.get = function (index, count, success) {
+				$timeout(function () {
+					var result = [];
+					for (var i = index; i <= index + count - 1; i++) {
+						result.push({
+							col1: i,
+							col2: 'item #' + i,
+							col3: (Math.random() < 0.5)
+						});
+					}
+					success(result);
+				}, 100);
+			};
+
+			$scope.datasource = datasource;
+
+			var cookieName = 'ui-scroll-grid-layout';
+
+			var defaultLayout = [
+				{index: 0, mapTo: 0, layout: {css: {}}},
+				{index: 1, mapTo: 1, layout: {css: {}}},
+				{index: 2, mapTo: 2, layout: {css: {}}}
+			];
+
+			var someLayout = [
+				{index: 0, mapTo: 0, layout: {css: {backgroundColor: '#f99'}}},
+				{index: 1, mapTo: 1, layout: {css: {backgroundColor: '#9f9'}}},
+				{index: 2, mapTo: 2, layout: {css: {backgroundColor: '#99f'}}}
+			];
+
+			$scope.layout = defaultLayout;
+
+			$scope.applyLayout = function () {
+				$scope.adapter.gridAdapter.applyLayout($scope.layout);
+			};
+
+			$scope.setSomeLayout = function () {
+				$scope.layout = someLayout;
+				$scope.applyLayout();
+			};
+
+			$scope.clearLayout = function () {
+				$scope.layout = defaultLayout;
+				$scope.applyLayout();
+			};
+
+			$scope.saveLayout = function () {
+				var layout = $scope.adapter.gridAdapter.getLayout();
+
+				var date = new Date();
+				date.setTime(date.getTime() + 30 * 24 * 3600 * 1000); // 30 days
+				document.cookie = cookieName + "=" + JSON.stringify(layout) + "; path=/;expires = " + date.toGMTString();
+			};
+
+			$scope.restoreLayout = function () {
+				var value = "; " + document.cookie;
+				var parts = value.split("; " + cookieName + "=");
+				var result;
+				if (parts.length != 2 || !(result = parts.pop().split(";").shift())) {
+					alert('Nothing to apply');
+					return;
+				}
+				
+				$scope.layout = JSON.parse(result);
+				$scope.applyLayout();
+			};
+
+		}
+	]);
