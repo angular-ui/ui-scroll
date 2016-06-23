a236a3055b4204217615520cc1d4a86785cc7e7f
d.alexanov <d.alexanov@gmail.com>
2016-06-23 20:32:53 +0300

    different item heights demo (not working!)

---
+++ demo/differentItemHeights/differentItemHeights.js
@@ -0,0 +1,29 @@
+angular.module('application', ['ui.scroll', 'ui.scroll.jqlite']).factory('datasource', [
+  '$log', '$timeout', function(console, $timeout) {
+    var get, max, min;
+    min = -50;
+    max = 100;
+
+    get = function(index, count, success) {
+      $timeout(function() {
+        var result = [];
+        var start = Math.max(min, index);
+        var end = Math.min(index + count - 1, max);
+        if (start <= end) {
+          for (var i = start; i <= end; i++) {
+            var j = i > 0 ? i : (-1) * i;
+            result.push({
+              text: "item #" + i,
+              height: 20 + (j%2) * 10
+            });
+          }
+        }
+         success(result);
+      }, 50);
+    };
+
+    return {
+      get: get
+    };
+  }
+]);
