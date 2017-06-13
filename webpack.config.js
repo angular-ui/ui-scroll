80656f0fbb173cb79375333e512833bdd366319e
dhilt <d.alexanov@gmail.com>
2017-06-13 21:15:51 +0300

    update webpack dependencies, remove webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -26,10 +26,7 @@ module.exports.config = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015']
-        }
+        loader: 'babel-loader?presets[]=es2015'
       }
     ]
   },
