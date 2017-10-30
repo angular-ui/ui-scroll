daf14fa4daeedf541b6ced75c31a90b85b4e8a71
dhilt <d.alexanov@gmail.com>
2017-10-30 21:43:29 +0300

    adapter sync demo refactoring

--- demo/adapterSync/adapterSync.js
+++ demo/adapterSync/adapterSync.js
@@ -1,148 +1,4 @@
-var app = angular.module('application', ['ui.scroll']);
-
-app.factory('Server', [
-  '$timeout', '$q', function ($timeout, $q) {
-
-    var ServerFactory = {
-
-      firstIndex: 1,
-
-      lastIndex: 40,
-
-      delay: 100,
-
-      data: [],
-
-      absIndex: 1,
-
-      generateId: function () {
-        var d = '-';
-        function S4() {
-          return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-        }
-        return (S4() + S4() + d + S4() + d + S4() + d + S4() + d + S4() + S4() + S4());
-      },
-
-      generateItem: function (index) {
-        return {
-          index: index,
-          id: this.generateId(),
-          content: 'Item #' + this.absIndex++
-        }
-      },
-
-      init: function () {
-        for (var i = this.firstIndex; i <= this.lastIndex; i++) {
-          this.data.push(this.generateItem(i));
-        }
-      },
-
-      getItem: function (index) {
-        for (var i = this.data.length - 1; i >= 0; i--) {
-          if (this.data[i].index === index) {
-            return this.data[i];
-          }
-        }
-      },
-
-      returnDeferredResult: function (result) {
-        var deferred = $q.defer();
-        $timeout(function () {
-          deferred.resolve(result);
-        }, this.delay);
-        return deferred.promise;
-      },
-
-      request: function (index, count) {
-        var start = index;
-        var end = index + count - 1;
-        var item, result = {
-          items: []
-        };
-        if (start <= end) {
-          for (var i = start; i <= end; i++) {
-            if (item = this.getItem(i)) {
-              result.items.push(item);
-            }
-          }
-        }
-        return this.returnDeferredResult(result);
-      },
-
-      prependItem: function (params) {
-        var newItem = this.generateItem(--this.firstIndex);
-        newItem.content += params;
-        this.data.unshift(newItem);
-        return this.returnDeferredResult(newItem);
-      },
-
-      appendItem: function (params) {
-        var newItem = this.generateItem(++this.lastIndex);
-        newItem.content += params;
-        this.data.push(newItem);
-        return this.returnDeferredResult(newItem);
-      },
-
-      removeFirst: function () {
-        var firstItem = this.data.find(i => i.index === this.firstIndex);
-        if(!firstItem) {
-          return $q.reject();
-        }
-        return this.removeItemById(firstItem.id);
-      },
-
-      removeLast: function () {
-        var lastItem = this.data.find(i => i.index === this.lastIndex);
-        if(!lastItem) {
-          return $q.reject();
-        }
-        return this.removeItemById(lastItem.id);
-      },
-
-      removeItemById: function (itemId) {
-        var length = this.data.length;
-        for (var i = 0; i < length; i++) {
-          if (this.data[i].id === itemId) {
-            var indexRemoved = this.data[i].index;
-            if(indexRemoved > this.firstIndex) {
-              for (var j = i; j < length; j++) {
-                this.data[j].index--;
-              }
-            }
-            this.data.splice(i, 1);
-            this.setIndicies();
-            return this.returnDeferredResult(indexRemoved);
-          }
-        }
-        return this.returnDeferredResult(false);
-      },
-
-      setIndicies: function() {
-        if(!this.data.length) {
-          this.firstIndex = 1;
-          this.lastIndex = 1;
-          return;
-        }
-        this.firstIndex = this.data[0].index;
-        this.lastIndex = this.data[0].index;
-        for (var i = this.data.length - 1; i >= 0; i--) {
-          if(this.data[i].index > this.lastIndex) {
-            this.lastIndex = this.data[i].index;
-          }
-          if(this.data[i].index < this.firstIndex) {
-            this.firstIndex = this.data[i].index;
-          }
-        }
-      }
-    };
-
-    ServerFactory.init();
-
-    return ServerFactory;
-
-  }
-]);
-
+var app = angular.module('application', ['ui.scroll', 'server']);
 
 app.controller('mainController', [
   '$scope', 'Server', function ($scope, Server) {
@@ -218,5 +74,46 @@ app.controller('mainController', [
         }
       });
     };
+
+    ctrl.insertSome = function (indexToInsert) {
+      indexToInsert = parseInt(indexToInsert, 10);
+      var promises = [
+        Server.insertAfterIndex(indexToInsert, ' *** (1)'),
+        Server.insertAfterIndex(indexToInsert + 1, ' *** (2)'),
+        Server.insertAfterIndex(indexToInsert + 2, ' *** (3)')
+      ];
+      Promise.all(promises).then(function (result) {
+        if (result && result.length) {
+          // need to protect from null
+          var _result = [];
+          for(var i = 0; i < result.length; i++) {
+            if(result[i]) {
+              _result.push(result[i]);
+            }
+          }
+          if(_result.length) {
+            var item = getItemByIndex(indexToInsert);
+            if(item) {
+              _result.unshift(item);
+            }
+            ctrl.adapter.applyUpdates(indexToInsert, _result); 
+          }
+        }
+      });
+    };
+
+    function getItemByIndex(index) {
+      var foundItem = null;
+      // use Adapter.applyUpdates to get indexed item from Buffer
+      ctrl.adapter.applyUpdates(function (item) {
+        if (item.index === index) {
+          foundItem = item;
+        }
+      });
+      return foundItem;
+    }    
+
+    ctrl.datasource.minIndex = Server.firstIndex;
+    ctrl.datasource.maxIndex = Server.lastIndex;
   }
 ]);
