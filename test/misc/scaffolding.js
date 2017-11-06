16941abd860237fddb7a967917535aafb7e089e6
dhilt <d.alexanov@gmail.com>
2017-11-06 22:53:18 +0300

    splitted tests scaffolding procedure

--- test/misc/scaffolding.js
+++ test/misc/scaffolding.js
@@ -21,34 +21,6 @@ function createHtml(settings) {
     '</div>';
 }
 
-function createGridHtml (settings) {
-  var viewportStyle = ' style="height:' + (settings.viewportHeight || 200) + 'px"';
-  var columns = ['col0', 'col1', 'col2', 'col3'];
-
-  var html =
-    '<table ui-scroll-viewport ' + viewportStyle + ' >' +
-      '<thead style="display:block">' +
-        '<tr>';
-  columns.forEach(col => { html +=
-          '<th ui-scroll-th class="' + col + '">' + col + '</th>';
-  }); html +=
-        '</tr>' +
-      '</thead>' +
-      '<tbody class="grid">' +
-        '<tr ui-scroll="item in ' + settings.datasource + '" adapter="adapter">';
-        if(settings.rowTemplate) {
-          html += settings.rowTemplate;
-        } else {
-          columns.forEach(col => { html +=
-            '<td ui-scroll-td class="' + col + '">{{item.' + col + '}}</td>';
-          });
-        } html +=
-        '</tr>' +
-      '</tbody>' +
-    '</table>';
-  return html;
-}
-
 function finalize(scroller, options = {}, scope, $timeout) {
   scroller.remove();
 
@@ -90,32 +62,5 @@ function runTest(scrollSettings, run, options = {}) {
         finalize(scroller, options, scope, $timeout);
       }
     }
-  });
-}
-
-function runGridTest(scrollSettings, run, options = {}) {
-  inject(function($rootScope, $compile, $window, $timeout) {
-    var scroller = angular.element(createGridHtml(scrollSettings));
-    var scope = $rootScope.$new();
-
-    angular.element(document).find('body').append(scroller);
-    var head = angular.element(scroller.children()[0]);
-    var body = angular.element(scroller.children()[1]);
-
-    if (options.scope) {
-      angular.extend(scope, options.scope);
-    }
-
-    $compile(scroller)(scope);
-
-    scope.$apply();
-    $timeout.flush();
-
-    try {
-      run(head, body, scope, $timeout);
-    } finally {
-      finalize(scroller, options, scope, $timeout);
-    }
-
   });
 }
