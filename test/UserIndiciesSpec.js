b98ab04eaea44cea2ffe58ca981af9114f5af914
dhilt <d.alexanov@gmail.com>
2017-11-08 08:30:58 +0300

    User indicies spec refactoring + reload test

--- test/UserIndiciesSpec.js
+++ test/UserIndiciesSpec.js
@@ -1,93 +1,129 @@
 /*global describe, beforeEach, module, inject, it, expect, runTest */
-describe('uiScroll main/max indicies', function() {
+describe('uiScroll user min/max indicies.', () => {
   'use strict';
 
   beforeEach(module('ui.scroll'));
   beforeEach(module('ui.scroll.test.datasources'));
 
-  describe('user min and max indexes', function() {
-    var viewportHeight = 120;
-    var itemHeight = 20;
-    var bufferSize = 3;
-    var userMinIndex = -99; // for 100 items
-    var userMaxIndex = 100;
+  var viewportHeight = 120;
+  var itemHeight = 20;
+  var bufferSize = 3;
+  var userMinIndex = -99; // for 100 items
+  var userMaxIndex = 100;
 
-    var scrollSettings = {
-      datasource: 'myInfiniteDatasource',
-      viewportHeight: viewportHeight,
-      itemHeight: itemHeight,
-      bufferSize: bufferSize
-    };
+  var scrollSettings = {
+    datasource: 'myInfiniteDatasource',
+    viewportHeight: viewportHeight,
+    itemHeight: itemHeight,
+    bufferSize: bufferSize,
+    adapter: 'adapter'
+  };
 
-    it('should calculate bottom padding element\'s height after user max index is set', function() {
-      var setMaxIndex;
+  describe('Setting\n', () => {
+
+    it('should calculate bottom padding element\'s height after user max index is set', () => {
+      let setMaxIndex;
       inject(function(myInfiniteDatasource) {
-        setMaxIndex = function() {
-          myInfiniteDatasource.maxIndex = userMaxIndex;
-        };
+        setMaxIndex = () =>myInfiniteDatasource.maxIndex = userMaxIndex;
       });
       runTest(scrollSettings,
-        function(viewport) {
-          var bottomPaddingElement = angular.element(viewport.children()[viewport.children().length - 1]);
+        (viewport) => {
+          const bottomPaddingElement = angular.element(viewport.children()[viewport.children().length - 1]);
           expect(viewport.scrollTop()).toBe(itemHeight * bufferSize);
 
           setMaxIndex();
 
-          var virtualItemsAmount = userMaxIndex - (viewportHeight / itemHeight) - bufferSize;
+          const virtualItemsAmount = userMaxIndex - (viewportHeight / itemHeight) - bufferSize;
           expect(bottomPaddingElement.height()).toBe(itemHeight * virtualItemsAmount);
           expect(viewport.scrollTop()).toBe(itemHeight * bufferSize);
         }
       );
     });
 
-    it('should calculate top padding element\'s height after user min index is set', function() {
-      var setMinIndex;
+    it('should calculate top padding element\'s height after user min index is set', () => {
+      let setMinIndex;
       inject(function(myInfiniteDatasource) {
-        setMinIndex = function() {
-          myInfiniteDatasource.minIndex = userMinIndex;
-        };
+        setMinIndex = () => myInfiniteDatasource.minIndex = userMinIndex;
       });
       runTest(scrollSettings,
-        function(viewport) {
-          var topPaddingElement = angular.element(viewport.children()[0]);
+        (viewport) => {
+          const topPaddingElement = angular.element(viewport.children()[0]);
           expect(viewport.scrollTop()).toBe(itemHeight * bufferSize);
 
           setMinIndex();
 
-          var virtualItemsAmount = (-1) * userMinIndex - bufferSize + 1;
+          const virtualItemsAmount = (-1) * userMinIndex - bufferSize + 1;
           expect(topPaddingElement.height()).toBe(itemHeight * virtualItemsAmount);
           expect(viewport.scrollTop()).toBe(itemHeight * ((-1) * userMinIndex + 1));
         }
       );
     });
 
-    it('should work with maxIndex pre-set on datasource', function() {
+  });
+
+  describe('Pre-setting\n', () => {
+
+    it('should work with maxIndex pre-set on datasource', () => {
       inject(function(myInfiniteDatasource) {
         myInfiniteDatasource.maxIndex = userMaxIndex;
       });
       runTest(scrollSettings,
-        function(viewport) {
-          var bottomPaddingElement = angular.element(viewport.children()[viewport.children().length - 1]);
-          var virtualItemsAmount = userMaxIndex - (viewportHeight / itemHeight) - bufferSize;
+        (viewport) => {
+          const bottomPaddingElement = angular.element(viewport.children()[viewport.children().length - 1]);
+          const virtualItemsAmount = userMaxIndex - (viewportHeight / itemHeight) - bufferSize;
           expect(bottomPaddingElement.height()).toBe(itemHeight * virtualItemsAmount);
           expect(viewport.scrollTop()).toBe(itemHeight * bufferSize);
         }
       );
     });
 
-    it('should work with minIndex pre-set on datasource', function() {
+    it('should work with minIndex pre-set on datasource', () => {
       inject(function(myInfiniteDatasource) {
         myInfiniteDatasource.minIndex = userMinIndex;
       });
       runTest(scrollSettings,
-        function(viewport) {
-          var topPaddingElement = angular.element(viewport.children()[0]);
-          var virtualItemsAmount = (-1) * userMinIndex - bufferSize + 1;
+        (viewport) => {
+          const topPaddingElement = angular.element(viewport.children()[0]);
+          const virtualItemsAmount = (-1) * userMinIndex - bufferSize + 1;
           expect(topPaddingElement.height()).toBe(itemHeight * virtualItemsAmount);
           expect(viewport.scrollTop()).toBe(itemHeight * ((-1) * userMinIndex + 1));
         }
       );
     });
+
+  });
+
+  describe('Reload\n', () => {
+
+    it('should persist user maxIndex after reload', () => {
+      inject(function(myInfiniteDatasource) {
+        myInfiniteDatasource.maxIndex = userMaxIndex;
+      });
+      runTest(scrollSettings,
+        (viewport, scope) => {
+          scope.adapter.reload();
+          const bottomPaddingElement = angular.element(viewport.children()[viewport.children().length - 1]);
+          const virtualItemsAmount = userMaxIndex - (viewportHeight / itemHeight) - bufferSize;
+          expect(bottomPaddingElement.height()).toBe(itemHeight * virtualItemsAmount);
+          expect(viewport.scrollTop()).toBe(itemHeight * bufferSize);
+        }
+      );
+    });
+
+    // it('should persist user minIndex after reload', () => {
+    //   inject(function(myInfiniteDatasource) {
+    //     myInfiniteDatasource.minIndex = userMinIndex;
+    //   });
+    //   runTest(scrollSettings,
+    //     (viewport, scope) => {
+    //       scope.adapter.reload();
+    //       const topPaddingElement = angular.element(viewport.children()[0]);
+    //       const virtualItemsAmount = (-1) * userMinIndex - bufferSize + 1;
+    //       expect(topPaddingElement.height()).toBe(itemHeight * virtualItemsAmount);
+    //       expect(viewport.scrollTop()).toBe(itemHeight * ((-1) * userMinIndex + 1));
+    //     }
+    //   );
+    // });
 
   });
 
