07e7047f60f33ca1b4d11b19801f29054f710576
Philippe Riand <priand@salesforce.com>
2019-08-16 17:10:49 -0400

    compiled files

--- dist/ui-scroll.js
+++ dist/ui-scroll.js
@@ -1,7 +1,7 @@
 /*!
  * angular-ui-scroll
  * https://github.com/angular-ui/ui-scroll
- * Version: 1.7.4 -- 2019-08-16T21:00:27.398Z
+ * Version: 1.7.4 -- 2019-08-16T21:09:14.732Z
  * License: MIT
  */
 /******/ (function(modules) { // webpackBootstrap
