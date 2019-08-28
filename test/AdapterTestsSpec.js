b8b512dd789495d65c4b3119db1df842bfed993b
dhilt <d.alexanov@gmail.com>
2019-08-28 05:31:44 +0300

    buffer cleanup when eof and immutableTop

--- test/AdapterTestsSpec.js
+++ test/AdapterTestsSpec.js
@@ -1611,47 +1611,74 @@ describe('uiScroll', function () {
   });
 
   describe('buffer cleanup', function () {
-    var scrollSettings = { datasource: 'myEdgeDatasource', adapter: 'adapter', viewportHeight: 60, itemHeight: 20, padding: 0.3, startIndex: 3, bufferSize: 3 };
+    var scrollSettings = {
+      datasource: 'myEdgeDatasource',
+      adapter: 'adapter',
+      viewportHeight: 60,
+      itemHeight: 20,
+      padding: 0.3,
+      startIndex: 3,
+      bufferSize: 3
+    };
 
     injectDatasource('myEdgeDatasource');
 
-    var cleanBuffer = function (scope) {
+    var cleanBuffer = function (datasource, scope, applyUpdateOptions) {
       var get = datasource.get;
       var removedItems = [];
+      var first = -5;
       // sync the datasource
       datasource.get = function (index, count, success) {
-        get(index, count, function (result) {
-          result = result.filter(function (item) {
-            return removedItems.indexOf(item) === -1;
-          });
-          success(result);
-        });
+        var removedIndex = removedItems.indexOf('item' + index);
+        if (removedIndex !== -1) {
+          // todo consider mutable-top case
+          index += removedItems.length - removedIndex;
+        }
+        get(index, count, success);
       };
       // clean up the buffer
-      scope.adapter.applyUpdates(
-        function (item) {
-          removedItems.push(item);
-          return [];
-        }, {
-          immutableTop: true
-        }
-      );
+      scope.adapter.applyUpdates(function (item) {
+        removedItems.push(item);
+        return [];
+      }, applyUpdateOptions);
     };
 
-    it('should be consistent on forward direction with immutabeTop', function () {
+    it('should be consistent on forward direction when eof with immutabeTop', function () {
       runTest(scrollSettings,
         function (viewport, scope) {
           expect(scope.adapter.isBOF()).toBe(false);
           expect(scope.adapter.isEOF()).toBe(true);
 
           // remove items 0..6 items form -5..6 datasource
-          cleanBuffer(scope);
+          cleanBuffer(datasource, scope, { immutableTop: true });
 
+          // result [-5..-1]
           expect(scope.adapter.isBOF()).toBe(true);
           expect(scope.adapter.isEOF()).toBe(true);
           expect(scope.adapter.bufferFirst).toBe('item-5');
           expect(scope.adapter.bufferLast).toBe('item-1');
           expect(scope.adapter.bufferLength).toBe(5);
+        }
+      );
+    });
+
+    it('should be consistent on forward direction when not eof with immutabeTop', function () {
+      scrollSettings.startIndex = -1;
+      scrollSettings.viewportHeight = 40;
+      runTest(scrollSettings,
+        function (viewport, scope) {
+          expect(scope.adapter.isBOF()).toBe(false);
+          expect(scope.adapter.isEOF()).toBe(false);
+
+          // remove items -4..1 items form -5..6 datasource
+          cleanBuffer(datasource, scope, { immutableTop: true });
+
+          // result [-5, 2, 3, 4]
+          expect(scope.adapter.isBOF()).toBe(true);
+          expect(scope.adapter.isEOF()).toBe(false);
+          expect(scope.adapter.bufferFirst).toBe('item-5');
+          expect(scope.adapter.bufferLast).toBe('item4');
+          expect(scope.adapter.bufferLength).toBe(4);
         }
       );
     });
